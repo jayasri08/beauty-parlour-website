e1a673893d8c3976873328d14bf94f648e57ab57
jayasri08 <95022741+jayasri08@users.noreply.github.com>
2023-11-24 15:17:05 +0530

    Add files via upload

---
+++ JAYASRI-PROJECT/salon.js
@@ -0,0 +1,27 @@
+//show nav bar
+const nav=document.querySelector('.nav-menu')
+const toggle=document.querySelector('.nav-toggle')
+toggle.onclick=function(){
+    nav.classList.toggle('show-nav')
+}
+
+//remove navbar
+
+const navLink=document.querySelectorAll('nav-link')
+
+function linkAction(){
+    const navMenu=document.querySelector('.nav-menu')
+    navMenu.classList.remove('show-nav')
+}
+navLink.forEach(n=>n.addEventListener('click',linkAction))
+
+//change active color
+
+const linkColor=document.querySelectorAll('.nav-link')
+function colorLink(){
+    if(linkColor){
+        linkColor.forEach(L=>L.classList.remove('active'))
+        this.classList.add('active')
+    }
+}
+linkColor.forEach(L=>L.addEventListener('click',colorLink))
